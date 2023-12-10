63c2ca57377cb630a1db84f43d89787b889a79ac
hmasood21955 <hmasood878@gmail.com>
2023-12-10 21:27:01 +0500

    Test cases for getAirDistance 2

--- utilsTester.c
+++ utilsTester.c
@@ -9,3 +9,8 @@ int main() {
     printf("Test Case 1:\nExpected: %.2f km\nActual: %.2f km\n", expectedDistance1, airDistance1);
     printf("Result: %s\n\n", (fabs(airDistance1 - expectedDistance1) < 0.01) ? "Passed" : "Failed");
 
+    double airDistance2 = getAirDistance(40.7128, -74.0060, 34.0522, -118.2437);
+    double expectedDistance2 = 3944.27;
+    printf("Test Case 2:\nExpected: %.2f km\nActual: %.2f km\n", expectedDistance2, airDistance2);
+    printf("Result: %s\n\n", (fabs(airDistance2 - expectedDistance2) < 0.01) ? "Passed" : "Failed");
+
